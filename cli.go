5324eca9e5cf7b6e08ec377f25fc788beb7041d4
moceviciusda <moceviciusdarius@gmail.com>
2024-09-01 00:58:18 +0300

    use readline package for interactivity

--- cli.go
+++ cli.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"io"
 	"strings"
 
+	"github.com/chzyer/readline"
 	"github.com/moceviciusda/pokeCLIpse-client/internal/serverapi"
 )
 
@@ -13,26 +13,62 @@ type config struct {
 	apiClient serverapi.Client
 }
 
+type completer struct {
+	commands map[string]cliCommand
+}
+
+func (c *completer) Do(line []rune, pos int) (newLine [][]rune, length int) {
+	for cmd := range c.commands {
+		if strings.HasPrefix(cmd, string(line)) {
+			newLine = append(newLine, []rune(cmd[pos:]))
+		}
+	}
+
+	return
+}
+
 func startCLI(cfg *config) {
 	commands := getCommands()
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("PokeCLIpse > ")
-		scanner.Scan()
 
-		input := scanner.Text()
-		if len(input) == 0 {
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "PokeCLIpse > ",
+		HistoryFile:     "/tmp/pokeclipse_history.txt",
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+		AutoComplete:    &completer{commands},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
 			continue
 		}
-		inputWords := strings.Fields(input)
 
+		rl.SaveHistory(line)
+
+		inputWords := strings.Fields(line)
 		command, ok := commands[inputWords[0]]
 		if !ok {
-			fmt.Println("invalid command:", input, "\ntype 'help' to see all available commands")
+			fmt.Println("invalid command:", line, "\ntype 'help' to see all available commands")
 			continue
 		}
 
-		err := command.callback(cfg, inputWords[1:]...)
+		err = command.callback(cfg, inputWords[1:]...)
 		if err != nil {
 			fmt.Println(err)
 		}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/moceviciusda/pokeCLIpse-client
 
 go 1.22.6
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/chzyer/readline v1.5.1
+	github.com/joho/godotenv v1.5.1
+)
+
+require golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
